aaac262021cf15f06cb8aa25d2eb7e12cec77717
ximena <ximena.aleja021@outlook.com>
2024-11-04 21:28:36 -0500

    Cuando el número es válido (está dentro del rango 1-10), el programa muestra el mensaje (Gracias)

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 let numero;
 numero = parseInt(prompt("Introduce un número entre 1 y 10:"));
-if (numero < 1 || numero > 10) 
+if (numero < 1 || numero > 10) {
+    alert("¡Número inválido! Por favor, ingresa un número entre 1 y 10.");
+    while (numero < 1 || numero > 10); {
+    }
+  }
